ca2d2b73c9e5f1f5ef29764949e1af893a6dc13f
akshathwalia-tech <akshathwalia@gmail.com>
2026-08-17 01:38:18 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (97.04%) - LeetSync

---
+++ 67-add-binary/add-binary.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    string addBinary(string a, string b) {
+        string result = "";
+    int i = a.length() - 1;
+    int j = b.length() - 1;
+    int carry = 0;
+
+    while (i >= 0 || j >= 0 || carry > 0) {
+        if (i >= 0) {
+            carry += a[i] - '0';
+            i--;
+        }
+        if (j >= 0) {
+            carry += b[j] - '0';
+            j--;
+        }
+
+        result += (carry % 2) + '0';
+        carry /= 2;
+    }
+
+    reverse(result.begin(), result.end());
+    return result;
+    }
+};
