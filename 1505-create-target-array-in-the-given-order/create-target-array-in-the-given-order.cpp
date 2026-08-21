d9fbee7abeccd34126eb97fd4fba56376585a5e3
akshathwalia-tech <akshathwalia@gmail.com>
2026-08-21 18:24:21 +0530

    Time: 0 ms (100.00%) | Memory: 11.4 MB (29.37%) - LeetSync

---
+++ 1505-create-target-array-in-the-given-order/create-target-array-in-the-given-order.cpp
@@ -0,0 +1,14 @@
+#include <vector>
+
+class Solution {
+public:
+    std::vector<int> createTargetArray(std::vector<int>& nums, std::vector<int>& index) {
+        std::vector<int> target;
+
+        for (int i = 0; i < nums.size(); i++) {
+            target.insert(target.begin() + index[i], nums[i]);
+        }
+
+        return target;
+    }
+};
