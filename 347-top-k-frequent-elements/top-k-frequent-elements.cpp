4ad84ca0e4d4f2d2fca535cd4e786e8227b72404
akshathwalia-tech <akshathwalia@gmail.com>
2026-08-23 02:23:09 +0530

    Time: 2 ms (58.11%) | Memory: 18 MB (70.41%) - LeetSync

---
+++ 347-top-k-frequent-elements/top-k-frequent-elements.cpp
@@ -0,0 +1,33 @@
+#include <vector>
+#include <unordered_map>
+#include <queue>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        unordered_map<int, int> freq;
+        for (int num : nums) {
+            freq[num]++;
+        }
+
+        // Min-heap ordered by frequency: {frequency, num}
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> minHeap;
+
+        for (auto& [num, count] : freq) {
+            minHeap.push({count, num});
+            if (minHeap.size() > k) {
+                minHeap.pop(); // Remove element with smallest frequency
+            }
+        }
+
+        vector<int> result;
+        while (!minHeap.empty()) {
+            result.push_back(minHeap.top().second);
+            minHeap.pop();
+        }
+
+        return result;
+    }
+};
