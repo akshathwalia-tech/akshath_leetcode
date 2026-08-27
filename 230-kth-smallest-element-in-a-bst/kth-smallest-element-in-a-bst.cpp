9adcf3d382da0a7f2620b84a3e0a42baef01d301
akshathwalia-tech <akshathwalia@gmail.com>
2026-08-27 18:30:30 +0530

    Time: 0 ms (100.00%) | Memory: 24.4 MB (66.71%) - LeetSync

---
+++ 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.cpp
@@ -0,0 +1,63 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+ 
+class Solution {
+public:
+    // Function to find the kth smallest element
+    int kthSmallest(TreeNode* root, int k) {
+        this->k = k;
+        this->result = -1;
+        inorder(root);
+        return result;
+    }
+
+    // Function to find the kth largest element
+    int kthLargest(TreeNode* root, int k) {
+        this->k = k;
+        this->result = -1;
+        reverse_inorder(root);
+        return result;
+    }
+
+    // Function to return kth smallest and kth largest elements
+    vector<int> kLargesSmall(TreeNode* root, int k) {
+        return {kthSmallest(root, k), kthLargest(root, k)};
+    }
+
+private:
+    int k;
+    int result;
+
+    // Helper function for inorder traversal
+    void inorder(TreeNode* node) {
+        if (node != nullptr) {
+            inorder(node->left);
+            if (--k == 0) {
+                result = node->val;
+                return;
+            }
+            inorder(node->right);
+        }
+    }
+
+    // Helper function for reverse inorder traversal
+    void reverse_inorder(TreeNode* node) {
+        if (node != nullptr) {
+            reverse_inorder(node->right);
+            if (--k == 0) {
+                result = node->val;
+                return;
+            }
+            reverse_inorder(node->left);
+        }
+    }
+};
