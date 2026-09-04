bfbf8194fb883303467f3e6e3a0f71d3e1c30661
akshathwalia-tech <akshathwalia@gmail.com>
2026-09-04 17:23:38 +0530

    Time: 500 ms (58.64%) | Memory: 243.4 MB (46.52%) - LeetSync

---
+++ 493-reverse-pairs/reverse-pairs.cpp
@@ -0,0 +1,59 @@
+#include <vector>
+// what we use here is modifies merge sort where before sorting or putting values at there right places, we chek the condition
+// given to  us in the question and adjust aour count according to that.
+class Solution {
+private:
+    int countPairs(std::vector<int>& nums, int low, int mid, int high) {
+        int right = mid + 1;
+        int count = 0;
+
+        for (int i = low; i <= mid; ++i) {
+            while (right <= high && (long long)nums[i] > 2LL * nums[right]) {
+                right++;
+            }
+            count += (right - (mid + 1));
+        }
+
+        return count;
+    }
+
+    void merge(std::vector<int>& nums, int low, int mid, int high) {
+        std::vector<int> temp;
+        int left = low;
+        int right = mid + 1;
+
+        while (left <= mid && right <= high) {
+            if (nums[left] <= nums[right]) {
+                temp.push_back(nums[left++]);
+            } else {
+                temp.push_back(nums[right++]);
+            }
+        }
+
+        while (left <= mid) temp.push_back(nums[left++]);
+        while (right <= high) temp.push_back(nums[right++]);
+
+        for (int i = low; i <= high; ++i) {
+            nums[i] = temp[i - low];
+        }
+    }
+
+    int mergeSort(std::vector<int>& nums, int low, int high) {
+        if (low >= high) return 0;
+
+        int mid = low + (high - low) / 2;
+        int count = 0;
+
+        count += mergeSort(nums, low, mid);
+        count += mergeSort(nums, mid + 1, high);
+        count += countPairs(nums, low, mid, high);
+        merge(nums, low, mid, high);
+
+        return count;
+    }
+
+public:
+    int reversePairs(std::vector<int>& nums) {
+        return mergeSort(nums, 0, nums.size() - 1);
+    }
+};
